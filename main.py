7cca34b2418197f7925b6a20d48881130243b29b
Varun Agrawal <varagrawal@gmail.com>
2019-07-05 14:56:03 -0400

    refactor of main

--- main.py
+++ main.py
@@ -6,13 +6,13 @@ import json
 
 import dateutil.parser
 
-from spamalyzer import MESSAGE_FILENAME, google_service, mail
+from spamalyzer import MESSAGE_FILENAME, google_service, mail, statistics
 
 
 def print_mail(idx, email):
     """Pretty print the email."""
     print("{0:3}: {1:<30.30} | {2:<50} \"{3:60.60}\" {4:>35}".format(idx, email.sender,
-                                                                     email.sender_email,
+                                                                     email.sender_address,
                                                                      email.subject,
                                                                      str(email.datetime)))
 
@@ -24,14 +24,20 @@ def main():
 
     next_page_token = None
     idx = 0
-    start_date = dateutil.parser.parse("01 Jul 2017 00:00:00 +0000")
+    start_date = dateutil.parser.parse("01 Jun 2013 00:00:00 +0000")
+
     keep_looping = True
+
+    emails = []
+
+    user = service.users().getProfile(userId="me")
 
     with open(MESSAGE_FILENAME, "w+") as messages_file:
         while keep_looping:
             # A dict with all the latest email IDs
             results = service.users().messages().list(userId="me",
                                                       pageToken=next_page_token).execute()
+
             next_page_token = results["nextPageToken"]
             messages = results.get('messages', [])
 
@@ -49,14 +55,23 @@ def main():
                     keep_looping = False
                     break
 
-                print("{0:4}".format(idx), email)
+                # ignore emails that are replies from self.
+                if email.sender_address == user['emailAddress']:
+                    continue
+
+                if email.is_unread:
+                    emails.append(email.sender_address)
+                    print("{0:4}".format(idx), email)
 
                 # Save the JSON to a newline
-                json.dump(email.dict(), messages_file)
-                messages_file.write("\n")
+                # json.dump(email.dict(), messages_file)
+                # messages_file.write("\n")
+
 
     print("Done going through email!")
 
+    counter = statistics.get_count(emails)
+    print(counter.most_common(15))
 
 if __name__ == "__main__":
     main()
